b655bede5e35369c74a69b2cccdccbf4ea2050d4
Мария <mahadrum@yandex.ru>
2023-03-07 14:05:05 +0300

    Отобразили ТОР10 в ListView

--- Top10FMCS/WindowsTop10.Designer.cs
+++ Top10FMCS/WindowsTop10.Designer.cs
@@ -29,18 +29,34 @@
         private void InitializeComponent()
         {
             this.listViewTop10 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // listViewTop10
             // 
+            this.listViewTop10.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
             this.listViewTop10.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewTop10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listViewTop10.HideSelection = false;
             this.listViewTop10.Location = new System.Drawing.Point(0, 0);
             this.listViewTop10.Name = "listViewTop10";
             this.listViewTop10.Size = new System.Drawing.Size(381, 450);
             this.listViewTop10.TabIndex = 0;
             this.listViewTop10.UseCompatibleStateImageBehavior = false;
-            this.listViewTop10.View = System.Windows.Forms.View.List;
+            this.listViewTop10.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Исполнитель";
+            this.columnHeader1.Width = 88;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "голосов";
+            this.columnHeader2.Width = 137;
             // 
             // WindowsTop10
             // 
@@ -58,5 +74,7 @@
         #endregion
 
         private System.Windows.Forms.ListView listViewTop10;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
--- Top10FMCS/WindowsTop10.cs
+++ Top10FMCS/WindowsTop10.cs
@@ -18,10 +18,10 @@ namespace Top10FMCS
             InitializeComponent();
         }
 
-        List<string> GetTop10()
+        List<string> GetListFromFile(string fileName)
         {
             List<string> lst = new List<string>();
-            StreamReader fileVotes = new StreamReader("votes.txt");
+            StreamReader fileVotes = new StreamReader(fileName);
             while (!fileVotes.EndOfStream)
             {
                 string s = fileVotes.ReadLine();
@@ -31,11 +31,32 @@ namespace Top10FMCS
             return lst;
         }
 
+        List<int> CountTop10(List<string> votes, List<string> songs)
+        {
+            List<int> count = new List<int>();
+            foreach (var song in songs)
+            {
+                int k = 0;
+                foreach (var vote in votes)
+                    if (song == vote) k++;
+                count.Add(k);
+            }
+            return count;
+        }
+
         private void WindowsTop10_Load(object sender, EventArgs e)
         {
-            List<string> top10 = GetTop10();
-            foreach (var s in top10)
-                listViewTop10.Items.Add(s);
+            List<string> votes = GetListFromFile("votes.txt");
+            List<string> songs = GetListFromFile("songs.txt");
+            List<int> top10 = CountTop10(votes, songs);
+            for(int i = 0; i < songs.Count; i++)
+            {
+                //listViewTop10.Items.Add(s);
+                ListViewItem data = new ListViewItem(songs[i]);
+                data.SubItems.Add(top10[i].ToString());
+                listViewTop10.Items.Add(data);
+            }
+            
         }
     }
 }
